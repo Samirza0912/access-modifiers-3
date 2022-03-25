948fc7df61b9aa5081e77fad3590712620e90e19
Samirza0912 <samirza@code.edu.az>
2022-03-25 06:43:29 +0400

    fix:i fixed some bugs

---
+++ test/Models/Userclass.cs
@@ -0,0 +1,71 @@
+﻿using System;
+namespace test.Models
+{
+    public class Userclass
+    {
+        private string _username;
+        private int _age;
+        private string _password;
+
+       
+        public string username { get { return _username; } set { _username = value; } }
+
+        public int age
+        { get
+         {
+                return _age;
+         }
+          set 
+         {
+                if (value<=0)
+                {
+                    Console.WriteLine("age cannot be negative!");
+                    return;
+                }
+                _age = value;
+         }
+        }
+
+        public string pass {
+            get
+            {
+                return _password;
+            }
+            set
+            {
+                if (value.Length!=0 && value.Length >= 8  && Cases(value) == true)
+                {
+                    Console.WriteLine("password be set");
+                    _password = value;
+                    return;
+                    
+                }
+                Console.WriteLine("Password can not be less than 8 symbols or empty");
+                return;
+              
+                          }
+        }
+        public bool Cases (string pass)
+        {
+            bool res1 = false;
+            bool res2 = false;
+            for (int i = 0; i < pass.Length; i++)
+            {
+                if (char.IsUpper(pass[i])==true)
+                {
+                    res2 = true;
+                }
+                if (char.IsNumber(pass[i])==true)
+                {
+                    res1 = true;
+                }  
+            }
+             bool res = res1 && res2;
+            return res;
+
+        }
+        
+        
+
+    }
+}
---
+++ test/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using test.Models;
+
+namespace test
+{
+    class Program
+    {
+        /*User class
+
+- Username
+- Age
+- Password
+
+Username, password property-ləri olmadan user obyekti yaratmaq olmaz.
+
+Age propery-i mənfi ola bilməz.
+
+Password property-i bu şərtləri ödəməlidir:
+
+1. Boş ola bilməz
+2. Uzunluğu 8-dən kiçik ola bilməz
+3. İçində ən az bir böyük hərf olmalıdır
+4. İçində ən az bir rəqəm olmalıdır
+
+Üçüncü və dördüncü şərti ayrı bir private methodda yazıb gəlib propertyinin setində istifadə etmək lazımdır.
+*/
+        static void Main(string[] args)
+        {
+            do
+            {
+             Userclass user = new Userclass();
+            Console.Clear();
+            Console.Write("Please enter your username: ");
+            user.username = Console.ReadLine();
+            Console.Write("Please write your age: ");
+            user.age = int.Parse(Console.ReadLine());
+            Console.Write("Please enter your password: ");
+            user.pass = Console.ReadLine();
+           
+                Console.WriteLine("for next press to any key::");
+                Console.WriteLine("for exit press to esc::");
+            }
+            while (Console.ReadKey().Key!=ConsoleKey.Escape);
+        }
+    }
+}
